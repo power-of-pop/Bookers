60544a422a6652c82e0313f6ef6ede300a9d7b77
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2024-12-11 07:47:19 +0000

    [Update] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
 
   def show
     @books = Book.all
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :books
   get 'top' => 'homes#top'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
